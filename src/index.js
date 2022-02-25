ae06516c2dd6dcba6cf8bbfed3092109b239b480
JonJWong <wongjonathan01@gmail.com>
2022-02-25 14:46:18 -0500

    receptors working

--- src/index.js
+++ src/index.js
@@ -1,40 +1,45 @@
 window.addEventListener("DOMContentLoaded", (event) => {
   const playArea = require('./game/play_area.js');
-  const Arrow = require('./game/arrow.js')
+  const Arrow = require('./game/arrow.js');
+  const Receptor = require('./game/receptor.js');
 
   window.canvasEl = document.getElementById('game-canvas');
   window.ctx = canvasEl.getContext('2d');
 
   window.Arrow = Arrow;
   window.playArea = playArea;
+  window.Receptor = Receptor;
 
-  let leftOpt = {
-    direction: 'left',
-    imgUrl: '/assets/images/Arrow.png',
-    pos: [50, 50]
-  }
-  let downOpt = {
-    direction: 'down',
-    imgUrl: '/assets/images/Arrow.png',
-    pos: [125, 50]
-  }
-  let upOpt = {
-    direction: 'up',
-    imgUrl: '/assets/images/Arrow.png',
-    pos: [200, 50]
-  }
-  let rightOpt = {
-    direction: 'right',
-    imgUrl: '/assets/images/Arrow.png',
-    pos: [275, 50]
-  }
-  let left = new Arrow(leftOpt);
-  left.draw(ctx)
-  let down = new Arrow(downOpt);
-  down.draw(ctx)
-  let up = new Arrow(upOpt);
-  up.draw(ctx)
-  let right = new Arrow(rightOpt);
-  right.draw(ctx)
+  let rec = new Receptor(4);
+  rec.render(ctx);
+
+  // let leftOpt = {
+  //   direction: 'left',
+  //   imgUrl: '/assets/images/Arrow.png',
+  //   pos: [50, 50]
+  // }
+  // let downOpt = {
+  //   direction: 'down',
+  //   imgUrl: '/assets/images/Arrow.png',
+  //   pos: [125, 50]
+  // }
+  // let upOpt = {
+  //   direction: 'up',
+  //   imgUrl: '/assets/images/Arrow.png',
+  //   pos: [200, 50]
+  // }
+  // let rightOpt = {
+  //   direction: 'right',
+  //   imgUrl: '/assets/images/Arrow.png',
+  //   pos: [275, 50]
+  // }
+  // let left = new Arrow(leftOpt);
+  // left.draw(ctx)
+  // let down = new Arrow(downOpt);
+  // down.draw(ctx)
+  // let up = new Arrow(upOpt);
+  // up.draw(ctx)
+  // let right = new Arrow(rightOpt);
+  // right.draw(ctx)
   console.log('DOM fully loaded and parsed');
 })
