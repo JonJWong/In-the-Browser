a5a219ecbe28b325885150cd0dd2d809e0e94d4a
JonJWong <wongjonathan01@gmail.com>
2022-02-25 14:07:41 -0500

    working on adding receptors

--- src/game/receptors.js
+++ src/game/receptors.js
@@ -1,8 +1,16 @@
 const Arrow = require('./arrow.js');
 
 class Receptor extends Arrow {
-  constructor(receptorOpts) {
+  constructor(num) {
+    this.receptors;
+  }
 
+  addReceptors(num) {
+    let receptors = [];
+    let i = 0;
+    while (receptors.length < num) {
+      
+    }
   }
 }
 
