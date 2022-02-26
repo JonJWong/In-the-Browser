6cb8176fbbbd164e19bcf43926118b844c75faab
JonJWong <wongjonathan01@gmail.com>
2022-02-25 21:04:42 -0500

    working on parsing chart

--- src/game/game.js
+++ src/game/game.js
@@ -7,7 +7,14 @@ class Game {
     this.maxScore = gameOpts['numNotes'] * 5;
     this.targets = new Target(gameOpts['numTargets']);
     this.arrows = [];
+    this.chart = this.buildChartObj(gameOpts['chartDir'])
   }
+  
+  buildChartObj(dir) {
+    // let chart = fetch('../../assets/chart/dpc/dpc.txt').then(res => res.text()).then(text => console.log(text));
+    let chart = fetch(`${dir}`).then(res => res.text()).then(text => text)
+  }
+  // readline info
 }
 
 module.exports = Game;
