e7430f16a503b0b10887442f2d66e52199c956e8
JonJWong <wongjonathan01@gmail.com>
2022-03-02 10:05:25 -0500

    everything working, doing lifebar now

--- src/game/game_view.js
+++ src/game/game_view.js
@@ -7,9 +7,11 @@ class GameView {
     this.ctx = ctx;
     this.diff = 9;
     this.currVolume = .5;
+    this.darkened = 0;
+
     this.startButtonHandler = this.startButtonHandler.bind(this);
     this.startButton = document.getElementById('start')
-    this.darkened = 0;
+    this.restartGame = this.restartGame.bind(this);
   }
 
   startButtonHandler() {
@@ -47,7 +49,7 @@ class GameView {
           case 3:
             return 5300
           case 5:
-            return 3150
+            return 3180
           case 10:
             return 1540
         }
@@ -118,24 +120,27 @@ class GameView {
   }
 
   restartGame() {
+    ctx.clearRect(0, 0, 1280, 960);
     const menu = document.getElementById('information-display');
     const optMenu = document.getElementById('game-opts');
     const inGameOverlay = document.getElementById('in-game-overlay');
     const stepStats = document.getElementById('step-statistics-block');
-    const failScreen = document.getElementById('end-screen');
-
+    const judgeText = document.getElementById('judgement');
+    
     menu.style.display = "block";
     optMenu.style.display = "none";
     inGameOverlay.style.display = "none";
     stepStats.style.display = "none";
-    failScreen.style.display = "none";
-
+    judgeText.style.display = "none";
+    
     clearInterval(this.interval);
-
+    
     const gameOpts = Options.gameOpts();
     this.game = new Game(gameOpts);
     this.startButton = document.getElementById('start');
     this.startButton.textContent = "Start Game";
+    const failScreen = document.getElementById('end-screen');
+    failScreen.style.display = "none";
   }
 
   playAudio() {
@@ -150,8 +155,14 @@ class GameView {
   openCloseOpts() {
     const mainMenu = document.getElementById('information-display');
     const optsMenu = document.getElementById('game-opts');
-    optsMenu.style.display = optsMenu.style.display === 'none' ? 'none' : 'block';
-    mainMenu.style.display = mainMenu.style.display === 'none' ? 'block' : 'none';
+    let mainCurStyle = mainMenu.style['display'];
+    if (mainCurStyle === 'none') {
+      mainMenu.style.display = 'block';
+      optsMenu.style.display = 'none';
+    } else {
+      mainMenu.style.display = 'none';
+      optsMenu.style.display = 'block';
+    }
   }
 
   bindKeys() {
